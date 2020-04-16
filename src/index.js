efb11d484fe5ae4b3b662cf2c0539f4e32900c33
alexRhine22 <alextherhine@gmail.com>
2020-04-16 14:55:03 -0700

    added nav bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import _ from 'lodash';
 import {createNavBar} from './topnav'
 
-// alert('IDK WHAT I AM DOING');
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     createNavBar();
 });
